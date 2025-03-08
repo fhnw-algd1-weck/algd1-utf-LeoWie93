a7189bbd35a4d0db7d6a2d5221bfac07e8abadc0
Leonardo Wiedemeier <leonardo.wiedemeier@gmail.com>
2025-03-08 10:42:33 +0100

    feat: codePoint to utf

--- src/main/java/ch/fhnw/algd1/converters/utf8/UTF8Converter.java
+++ src/main/java/ch/fhnw/algd1/converters/utf8/UTF8Converter.java
@@ -3,37 +3,120 @@ package ch.fhnw.algd1.converters.utf8;
 /*
  * Created on 05.09.2014
  */
+
+import java.util.Arrays;
+
 /**
- * @author 
+ * @author
  */
 public class UTF8Converter {
-	public static byte[] codePointToUTF(int x) {
-		byte[] b = null;
-		// TODO allocate b in the right size (depending on x) and fill it with the
-		// UTF-8 encoding of code point x. b[0] shall contain the first byte.
-		return b;
-	}
 
-	public static int UTFtoCodePoint(byte[] bytes) {
-		if (isValidUTF8(bytes)) {
-			// TODO replace return statement below by code to return the code point
-			// UTF-8 encoded in array bytes. bytes[0] contains the first byte
-			return 0;
-		} else return 0;
-	}
+    // big if else. but i could not be bothered
+    public static byte[] codePointToUTF(int x) {
+        byte[] b = null;
 
-	private static boolean isValidUTF8(byte[] bytes) {
-		if (bytes.length == 1) return (bytes[0] & 0b1000_0000) == 0;
-		else if (bytes.length == 2) return ((bytes[0] & 0b1110_0000) == 0b1100_0000)
-				&& isFollowup(bytes[1]);
-		else if (bytes.length == 3) return ((bytes[0] & 0b1111_0000) == 0b1110_0000)
-				&& isFollowup(bytes[1]) && isFollowup(bytes[2]);
-		else if (bytes.length == 4) return ((bytes[0] & 0b1111_1000) == 0b1111_0000)
-				&& isFollowup(bytes[1]) && isFollowup(bytes[2]) && isFollowup(bytes[3]);
-		else return false;
-	}
+        String binaryString = intToBinaryString(x);
 
-	private static boolean isFollowup(byte b) {
-		return (b & 0b1100_0000) == 0b1000_0000;
-	}
+        if (binaryString.length() <= 7) {
+            b = new byte[1];
+            b[0] = (byte) binaryStringToInt(binaryString);
+
+        } else if (binaryString.length() <= 11) {
+            b = new byte[2];
+
+            String followUp = binaryString.substring(binaryString.length() - 6);
+            String main = binaryString.substring(0, binaryString.length() - 6);
+
+            b[1] = (byte) binaryStringToInt("10" + followUp);
+
+            int padding = 5 - main.length();
+            main = "110" + "0".repeat(padding) + main;
+            b[0] = (byte) binaryStringToInt(main);
+
+        } else if (binaryString.length() <= 16) {
+            b = new byte[3];
+
+            String followUpPart = binaryString.substring(binaryString.length() - 12);
+            b[2] = (byte) binaryStringToInt("10" + followUpPart.substring(followUpPart.length() - 6));
+            b[1] = (byte) binaryStringToInt("10" + followUpPart.substring(0, followUpPart.length() - 6));
+
+            String main = binaryString.substring(0, binaryString.length() - 12);
+            int padding = 4 - main.length();
+            main = "1110" + "0".repeat(padding) + main;
+            b[0] = (byte) binaryStringToInt(main);
+
+        } else if (binaryString.length() <= 21) {
+            b = new byte[4];
+
+            if (binaryString.length() == 17) {
+                binaryString = "0" + binaryString;
+            }
+
+            String followUpPart = binaryString.substring(binaryString.length() - 18);
+            String main = binaryString.substring(0, binaryString.length() - 18);
+
+            b[3] = (byte) binaryStringToInt("10" + followUpPart.substring(followUpPart.length() - 6));
+            b[2] = (byte) binaryStringToInt("10" + followUpPart.substring(6, binaryString.length() - 6));
+            b[1] = (byte) binaryStringToInt("10" + followUpPart.substring(0, 6));
+
+            int padding = 3 - main.length();
+            main = "11110" + "0".repeat(padding) + main;
+            b[0] = (byte) binaryStringToInt(main);
+
+        } else {
+            return new byte[]{0};
+        }
+
+        return b;
+    }
+
+    public static int UTFtoCodePoint(byte[] bytes) {
+        if (isValidUTF8(bytes)) {
+            // TODO replace return statement below by code to return the code point
+            // UTF-8 encoded in array bytes. bytes[0] contains the first byte
+
+            return 0;
+        } else {
+            return 0;
+        }
+    }
+
+    private static boolean isValidUTF8(byte[] bytes) {
+        if (bytes.length == 1) return (bytes[0] & 0b1000_0000) == 0;
+        else if (bytes.length == 2) return ((bytes[0] & 0b1110_0000) == 0b1100_0000)
+                && isFollowup(bytes[1]);
+        else if (bytes.length == 3) return ((bytes[0] & 0b1111_0000) == 0b1110_0000)
+                && isFollowup(bytes[1]) && isFollowup(bytes[2]);
+        else if (bytes.length == 4) return ((bytes[0] & 0b1111_1000) == 0b1111_0000)
+                && isFollowup(bytes[1]) && isFollowup(bytes[2]) && isFollowup(bytes[3]);
+        else return false;
+    }
+
+    private static boolean isFollowup(byte b) {
+        return (b & 0b1100_0000) == 0b1000_0000;
+    }
+
+    private static int binaryStringToInt(String binary) {
+        int x = 0;
+        for (int i = 0; i < binary.length(); i++) {
+            x = (x << 1) + (binary.charAt(i) == '0' ? 0 : 1);
+        }
+
+        return x;
+    }
+
+    private static String intToBinaryString(int x) {
+        String s = "";
+
+        while (x > 0) {
+            if (x % 2 == 1) {
+                s = "1" + s;
+            } else {
+                s = "0" + s;
+            }
+            x = x >> 1;
+        }
+
+        return s;
+    }
 }
